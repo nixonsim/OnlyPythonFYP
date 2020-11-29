41dcbeb0d1cac4d2cdc8bd425fdbaa8eaaab9847
Nixon Sim <18037956@myrp.edu.sg>
2020-11-30 02:37:42 +0800

    changes made to controllers

---
+++ Controllers/TemplateController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Security.Claims;
+using OnlyPythonFYP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlyPythonFYP.Controllers
+{
+    public class TemplateController : Controller
+    {
+        private AppDbContext _dbContext;
+
+        public TemplateController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            DbSet<Qntemplate> dbs = _dbContext.Qntemplate;
+            List<Qntemplate> model = dbs.ToList();
+            return View(model);
+        }
+
+        public IActionResult Display(int id)
+        {
+            DbSet<Qntemplate> dbs = _dbContext.Qntemplate;
+
+            Qntemplate qntemp = dbs.Where(mo => mo.Id == id)
+                                 .FirstOrDefault();
+
+            return View(qntemp);
+        }
+    }
+}
